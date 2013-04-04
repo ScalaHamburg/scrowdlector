5b6c2ff6c0f107d546381f0f9a919a973b3b6aa1
PTR <patrick.trapp@logica.com>
2013-04-04 09:50:04 +0200

    first shot (debugged)

--- app/hashing/TextBlockHash.scala
+++ app/hashing/TextBlockHash.scala
@@ -1,61 +1,66 @@
 package hashing
 
-class TextBlockHash {
+import org.apache.commons.lang.StringUtils
 
-  /**
-   * First approach to cut a text into some blocks that may be commented.
-   * A Block ends at file end or a sequence of newlines.
-   */
-  def blockify(text: String) = {
-    combineLines(text.linesWithSeparators.toStream)
-  }
 
-  def combineLines(lines: Stream[String]): Stream[String] = {
-    lines match {
-      case x #:: tail => combine(x, tail)
-      case strm => strm
-    }
-  }
-
-  def combine(block: String, stream: Stream[String]): Stream[String] = {
-    stream match {
-      case line #:: tail => {
-        line match {
-          case str => 
-            if(str.trim.length==0){
-            block #:: combine(block, tail)
-//            if (block.size == 0) {
-//          	combine(block, tail)
-          } else {
-            
-          	combine(block + line, tail)
-          }
-        }
-      }
-      case strm => block #:: strm
-    }
-  }
-
-}
 
 object TextBlockHash {
-  def main(args: Array[String]) {
-    val text = """
-  	  |Zeile 1
+	/**
+	 * First approach to cut a text into some blocks that may be commented.
+	 * A Block ends at file end or a sequence of newlines.
+	 */
+	def blockify(text: String) = {
+		def combineLines(lines: Stream[String]): Stream[String] = {
+				lines match {
+				case x #:: tail => combine(x, tail)
+				case strm       => strm
+				}
+		}
+		
+		def combine(block: String, stream: Stream[String]): Stream[String] = {
+				stream match {
+				case line #:: tail => {
+					
+					if (StringUtils.isBlank(line)) {
+						if (block.size == 0) {
+							combine("", tail)
+						} else {
+							block #:: combine("", tail)
+						}
+					} else {
+						
+						combine(block + line, tail)
+					}
+					
+				}
+				case strm => block #:: strm
+				}
+		}
+		combineLines(text.linesWithSeparators.toStream)
+	}
+	
+	def hashBlock(block:String) = {
+		(block, block.hashCode().toString)
+	}
+
+	def main(args: Array[String]) {
+    val text = """|Zeile 1
   	  |Zeile 2
   	  |Zeile 3
     	|
   	  |Block2
   	  |usw.
-  	  """.stripMargin
-      val test = new TextBlockHash
-      
-    	val ergebnis = test.blockify(text)
-    	ergebnis.foreach{s=>
-	      println(">>>>>")
-	      println(s)
-	      println("<<<<<")
-	    }
-    
+      |
+      |
+      |noch ein Block 
+      |(der dritte)""".stripMargin
+
+    val ergebnis = blockify(text).map(hashBlock(_))
+    ergebnis.foreach { s =>
+      println(">>>>>")
+      println(s)
+      println("<<<<<")
+    }
+
   }
 }
