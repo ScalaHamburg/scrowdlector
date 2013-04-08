d347dae46e1bd5bf245b268eed1f89faca642e41
Nils Schmidt <nsdn@gmx.de>
2013-04-08 11:31:30 +0200

    Added add-comment service method

--- app/service/InMemoryCommentService.scala
+++ app/service/InMemoryCommentService.scala
@@ -3,9 +3,13 @@ package service
 import model.Comment
 
 class InMemoryCommentService extends CommentService {
-  val comments = List(new Comment("Nice header!", "-707665951"), new Comment("Really nice header, indeed!!!", "-707665951"))
+  var comments = List(new Comment("Nice header!", "-707665951"), new Comment("Really nice header, indeed!!!", "-707665951"))
   
   def find(documentBlockIdentifier: String): Seq[Comment] = {
     comments.filter(_.documentBlockIdentifier == documentBlockIdentifier)
+  }
+  
+  def add(comment: String, documentBlockIdentifier: String) = {
+    comments = new Comment(comment, documentBlockIdentifier) :: comments
   }
 }
